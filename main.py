8defcd9a5c1d3b897de5ef219a6d9c8b3346fa2d
jjordanzlatanov <jjordanzlatanov@gmail.com>
2022-01-15 20:47:44 +0200

    Tests

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import requests
-from typing import List
 
-data = requests.post('https://tues2022.proxy.beeceptor.com/my/api/test')
-temperatures = []  # type: List[int]
-
+temperatures = []
 
 def mintemp():
     return temperatures[0]
@@ -17,6 +14,8 @@ def maxtemp():
     return temperatures[4]
 
 
+data = requests.post('https://tues2022.proxy.beeceptor.com/my/api/test')
+    
 for i in data.json()['data']:
     temp = i['temperature']
     temperatures.append(temp)
---
+++ test_temperature.py
@@ -0,0 +1,23 @@
+import requests
+from main import mintemp, mediumtemp, maxtemp
+
+data = requests.post('https://tues2022.proxy.beeceptor.com/my/api/test')
+temperatures = []
+
+for i in data.json()['data']:
+    temp = i['temperature']
+    temperatures.append(temp)
+
+
+temperatures.sort()
+
+def test_mintemp():
+    assert mintemp() == temperatures[0]
+
+
+def test_mediumtemp():
+    assert mediumtemp() == temperatures[2]
+
+
+def test_maxtemp():
+    assert maxtemp() == temperatures[4]
